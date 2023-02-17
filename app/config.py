583a3a51d4c91ac8bb6a11908109e9a166d51abb
boxgitee <17826806114@163.com>
2023-02-17 16:08:00 +0800

    ROOT_PATH

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ from pydantic import BaseSettings
 import os
 
 class Settings(BaseSettings):
-    ROOT_PATH:str = r"C:\hrp4demo" 
+    ROOT_PATH:str = os.path.abspath("../hrun_proj")
     LOG_LEVEL:str = "DEBUG"
     HRUN_EXCUTE_ARGS:list = ["hrun","-sW","ignore:Module already imported:pytest.PytestWarning","--save-tests","--log-level",LOG_LEVEL]
